f706f648243a50c566db6d4b84cad1111bab991c
LearnerSamuelX <samxiezx1115@gmail.com>
2020-05-06 22:25:04 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+// const routes = require('./routes/query')
+
+//PostGreSQL setup
+const Pool = require('pg').Pool
+//using pool for connection, not connect
+const pool = new Pool({
+  user: 'postgre',
+  host: 'localhost',
+  database: 'p2p',
+  password: 'Gogo11!5',
+  port: 5432,
+})
+const mainRouter = require('./routes/main')
+
+
+//dotenv installed
+
+//for POST request
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true,})) 
+
+app.set('view engine', 'ejs');
+app.set('views',__dirname + '/views')
+app.set('layout','layouts/layout')
+
+app.use('/', mainRouter)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
